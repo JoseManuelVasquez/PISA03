1cdfca038b0c672883035b743223b204e81fa59f
JoseManuelVasquez <vcjm2508@gmail.com>
2018-03-06 19:52:35 +0100

    First Commit

---
+++ ChappApp/app/src/main/java/edu/ub/pis2018/grup03/chappapp/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.ub.pis2018.grup03.chappapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
